bd9edaede4dd4786eebee690195d6228b83dbbf2
masRobins <robinson.maasiah@gmail.com>
2020-01-27 19:57:47 -0500

    Updates

---
+++ precache-manifest.57c4ec16132b3943815acf96be5f073d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825183ef9c58d68090907648fd6efbab",
+    "url": "/masRobins/monsters-search/index.html"
+  },
+  {
+    "revision": "c8249eff0503775ca1d7",
+    "url": "/masRobins/monsters-search/static/css/main.c70fd6ee.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/masRobins/monsters-search/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/masRobins/monsters-search/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8249eff0503775ca1d7",
+    "url": "/masRobins/monsters-search/static/js/main.6900d09d.chunk.js"
+  },
+  {
+    "revision": "8669a59b1a11c3ef8e8a",
+    "url": "/masRobins/monsters-search/static/js/runtime-main.75ae91f3.js"
+  }
+]);
